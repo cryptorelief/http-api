6100e2b6d56fdce22f620084721a60cdf8b516a3
Anuj <code@anuj.me>
2021-05-18 18:44:56 +0530

    work on GET request querying; refactoring for readability

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-import json
-from flask import Flask,request
+from flask import Flask, request
 from flask_sqlalchemy import sqlalchemy
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.exc import SQLAlchemyError as err
+from sqlalchemy.exc import SQLAlchemyError
 from credentials import USERNAME,PASSWORD,HOSTNAME,PORT,DB_NAME
-
+import pandas as pd
+from datetime import datetime
 app = Flask(__name__)
 
-pengine = sqlalchemy.create_engine("postgresql+psycopg2://{}:{}@{}:{}/{}".format(USERNAME,PASSWORD,HOSTNAME,PORT,DB_NAME))
+pengine = sqlalchemy.create_engine("postgresql+psycopg2://{}:{}@{}:{}/{}".format(USERNAME,PASSWORD,HOSTNAME,PORT,DB_NAME), pool_use_lifo=True, pool_pre_ping=True)
 Base = declarative_base()
 metadata = sqlalchemy.MetaData(pengine)
 metadata.reflect()
 
-# All the tables in the database (probably would be renamed in the future)
-
+# All tables that can be queried using the API
 class Demand(Base):
     __table__ = sqlalchemy.Table("Demand", metadata)
 
@@ -27,113 +26,153 @@ class Matches(Base):
 class Raw(Base):
     __table__ = sqlalchemy.Table("Raw", metadata)
 
-# session object to talk with the db
+# Session object to talk with the db
 Session = sqlalchemy.orm.sessionmaker(pengine)
 session = Session()
 
-# base_url = "api.covidbot.in"
-
-def get_results(table):
+def filter_data(table):
     r_dict = request.args.to_dict()
+    # special query parameters that are not table columns
+    verified_after = r_dict.pop("verified_after", None)
     after = r_dict.pop("after", None)
     before = r_dict.pop("before", None)
-    s = session.query(table).filter_by(**r_dict)
-    if after is not None:
-        s = s.filter(table.datetime>after)
-    if before is not None:
-        s = s.filter(table.datetime<before)
-    results = s.all()
-    return results
+    try:
+        verified_after = pd.to_datetime(verified_after)
+        after = pd.to_datetime(after)
+        before = pd.to_datetime(before)
+    except:
+        return "Invalid datetime format"
+    limit = r_dict.pop("limit", None)
+    try:
+        # construct the query
+        s = session.query(table).filter_by(**r_dict)
+        if after:
+            s = s.filter(table.last_updated>after)
+        if before:
+            s = s.filter(table.last_updated<before)
+        if verified_after:
+            s = s.filter(table.last_verified_on>=verified_after)
+        # apply sensible defaults for ordering
+        s = s.order_by(sqlalchemy.text('verified desc, last_verified_on desc, last_updated desc'))
+        # NOTE: limit has to be applied after ordering
+        if limit:
+            s = s.limit(limit)
+        # run query
+        results = s.all()
+        return results
+    except (SQLAlchemyError, ValueError) as e:
+        return str(e)
 
-def results_to_json(results):
-    if isinstance(results,list) is False:
-        return json.dumps(results)
-    all_results = [vars(result) for result in results]
-    final_result = []
-    for result in all_results:
-        final_result.append(dict([(k,v) for (k,v) in result.items() if not k.startswith("_")]))
-    final_result_json = json.dumps(final_result,indent=4,default=str)
-    return final_result_json
 
-def put_results(table):
+def insert_data(table):
     r_dict = request.args.to_dict()
     try:
         d = table(**r_dict)
         session.add(d)
         session.commit()
         results = [d]
-        return results,200
-    except Exception as e:
-        return (str(e)),400
+        return results
+    except (SQLAlchemyError, ValueError) as e:
+        return str(e)
 
-def update_results(table):
+
+def update_data(table):
     r_dict = request.args.to_dict()
     try:
-        s = session.query(Supply).filter_by(external_id=r_dict["external_id"]).first()
+        s = session.query(Supply).filter_by(id=r_dict["id"]).first()
         if s is None:
-            return [],400
+            return []
         for k in r_dict:
             s.k = r_dict[k]
         session.commit()
+    except (SQLAlchemyError, ValueError) as e:
+        return str(e)
+    else:
         results = [s]
-        return results,200
-    except Exception as e:
-        return (str(e)),400
+        return results
 
-@app.get("/request")
+
+def generate_response(results):
+    """Converts results (empty list, list of vars (incl. internal vars), or string) into a response-friendly object"""
+    # if result was a string, it's an error
+    if isinstance(results,str):
+        return {"error": results.replace("\"", "'")}, 400
+    elif not isinstance(results, list):
+        return {"error": "Invalid data received from server", "data": results}, 503
+    # get all vars/children of the SQLAlchemy object
+    all_results = [vars(result) for result in results]
+    # this also contains internal Python variables, so we remove those
+    final_result = []
+    for result in all_results:
+        final_result.append(dict([(k,v) for (k,v) in result.items() if not k.startswith("_")]))
+    return {"data": final_result}
+
+
+@app.get("/requests")
 def get_demand():
-    results = get_results(Demand)
-    return results_to_json(results)
+    results = filter_data(Demand)
+    return generate_response(results)
+
 
 @app.get("/supply")
 def get_supply():
-    results = get_results(Supply)
-    return results_to_json(results)
+    results = filter_data(Supply)
+    return generate_response(results)
+
 
 @app.get("/matches")
 def get_matches():
-    results = get_results(Matches)
-    return results_to_json(results)
+    results = filter_data(Matches)
+    return generate_response(results)
+
 
 @app.get("/raw")
 def get_raw():
-    results = get_results(Raw)
-    return results_to_json(results)
+    results = filter_data(Raw)
+    return generate_response(results)
 
-@app.put("/request")
+
+@app.put("/requests")
 def put_demand():
-    results,status_code = put_results(Demand)
-    return results_to_json(results),status_code
+    results,status_code = insert_data(Demand)
+    return generate_response(results)
+
 
 @app.put("/supply")
 def put_supply():
-    results,status_code = put_results(Supply)
-    return results_to_json(results),status_code
+    results,status_code = insert_data(Supply)
+    return generate_response(results)
+
 
 @app.put("/raw")
 def put_raw():
-    results,status_code = put_results(Raw)
-    return results_to_json(results),status_code
+    results,status_code = insert_data(Raw)
+    return generate_response(results)
+
 
 @app.put("/update/demand")
 def update_demand():
-    results,status_code = update_results(Demand)
-    return results_to_json(results)
+    results,status_code = update_data(Demand)
+    return generate_response(results)
+
 
 @app.put("/update/supply")
 def update_supply():
-    results,status_code = update_results(Supply)
-    return results_to_json(results)
+    results,status_code = update_data(Supply)
+    return generate_response(results)
+
 
 @app.put("/update/matches")
 def update_matches():
-    results,status_code = update_results(Matches)
-    return results_to_json(results)
+    results,status_code = update_data(Matches)
+    return generate_response(results)
+
 
 @app.put("/update/raw")
 def update_raw():
-    results,status_code = update_results(Raw)
-    return results_to_json(results)
+    results,status_code = update_data(Raw)
+    return generate_response(results)
+
 
 if __name__=="__main__":
     app.run(debug=True)
