28dae80c94d89db8666ec12f4af60297033ac68a
Anuj <code@anuj.me>
2021-06-01 18:12:14 +0530

    Added volunteer and location endpoints; changed base url; bugfixes; refactoring

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from flask_jwt_extended import create_access_token,get_jwt_identity,jwt_required
 from werkzeug.security import safe_str_cmp
 import pandas as pd
 from datetime import datetime
-from db import Demand, Supply, Raw, Matches, UserLog, Auth, Contact, get_session
+from db import Demand, Supply, Raw, Matches, UserLog, Auth, Contact, Locations, Volunteer
+from db import get_session
 from sqlalchemy.sql.expression import desc, nulls_last
 from credentials import JWT_KEY
 
@@ -15,6 +16,7 @@ app.config['JWT_SECRET_KEY'] = JWT_KEY
 app.config["JWT_TOKEN_LOCATION"] = ["headers", "query_string"]
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False
 app.config['JWT_REFRESH_TOKEN_EXPIRES'] = False
+app.config['APPLICATION_ROOT'] = "/core"
 jwt = JWTManager(app)
 
 
@@ -24,33 +26,64 @@ def obj_to_dict(obj):
 
 def search(table):
     args = request.args.to_dict()
-    # special query parameters that are not table columns
+    # special query parameters that are not table columns:
+    # verified_after, (updated) after, (updated) before, limit
     verified_after = args.pop("verified_after", None)
     after = args.pop("after", None)
     before = args.pop("before", None)
-        
+    # validate special fields
     try:
+        # `pd.to_datetime(None)` returns `None` so this is okay
         verified_after = pd.to_datetime(verified_after)
         after = pd.to_datetime(after)
         before = pd.to_datetime(before)
     except:
         return "Invalid datetime format"
-    limit = args.pop("limit", None)
+    limit = args.pop("limit", "")
+    if limit and not limit.isdigit():
+        return "limit must be an integer"
 
     try:
         # construct the query
         results = []
         with get_session() as session:
             s = session.query(table).filter_by(**args)
+            # optional/special query parameters
             if after:
-                s = s.filter(table.last_updated>after)
+                s = s.filter(table.last_updated>=after)
             if before:
                 s = s.filter(table.last_updated<before)
             if verified_after:
                 s = s.filter(table.last_verified_on>=verified_after)
-            # apply sensible defaults for ordering
+            # sensible defaults for ordering: verified, last_verified_on, last_updated
             if table == Supply:
                 s = s.order_by(nulls_last(desc(Supply.verified)), nulls_last(desc(Supply.last_verified_on)), nulls_last(desc(Supply.last_updated)))
+            # NOTE: limit has to be applied after ordering
+            if limit:
+                s = s.limit(limit)
+            # run query
+            results = s.all()
+            results = [obj_to_dict(result) for result in results]
+        return results
+    except (SQLAlchemyError, ValueError) as e:
+        return str(e)
+
+
+def location_search():
+    args = request.args.to_dict()
+    limit = args.pop("limit", "")
+    if limit and not limit.isdigit():
+        return "limit must be an integer"
+    name = args.pop("name", "")
+
+    try:
+        # construct the query
+        results = []
+        with get_session() as session:
+            s = session.query(Locations).filter_by(**args)
+            # optional/special query parameters
+            if name:
+                s = s.filter(Locations.name.ilike(name))
             # NOTE: limit has to be applied after ordering
             if limit:
                 s = s.limit(limit)
@@ -76,22 +109,26 @@ def insert_or_update(table):
 
 
 def insert(table, data):
-    tg_user_id = data.pop("tg_user_id", None)
-    user_handle = data.pop("tg_user_handle", None)
-    source = data.pop("source", None)
-    contact_args = {}
-    if tg_user_id:
-        contact_args['tg_user_id'] = tg_user_id
-    if user_handle:
-        contact_args['user_handle'] = user_handle
-    if source:
-        contact_args['source'] = source
+    source = data.pop("source", "").lower()
+    contact_lookup = {}
+    if source == "telegram":
+        tg_user_id = data.pop("tg_user_id", None)
+        tg_user_handle = data.pop("tg_user_handle", None)
+        if tg_user_id:
+            contact_lookup['tg_user_id'] = tg_user_id
+        if tg_user_handle:
+            contact_lookup['user_handle'] = tg_user_handle
+    # add source only if there's lookup data - otherwise skip
     try:
         results = []
         with get_session() as session:
             user = session.query(Auth).filter_by(id=get_jwt_identity()).first()
-            if contact_args:
-                contact = session.query(Contact).filter_by(**contact_args).first()
+            if not user:
+                return "Authentication failed"
+            if contact_lookup:
+                contact = session.query(Contact).filter_by(**contact_lookup).first()
+                if not contact:
+                    return f"Contact not found: {contact_lookup}"
                 data.update(contact=contact)
             record = table(**data)
             ## create User Log
@@ -150,29 +187,36 @@ def update(table, data, identifier):
             session.refresh(record)
             results = [obj_to_dict(record)]
         return results
-    except (SQLAlchemyError,  ValueError) as e:
+    except (SQLAlchemyError, ValueError) as e:
         return str(e)
 
 
 def find_matches():
     args = request.args.to_dict()
-    source = args.get("source", "")
-    if source.lower()=="telegram":
+    source = args.get("source", "").lower()
+    contact = None
+    # if source is telegram, we need to find/create the user
+    if source == "telegram":
         tg_user_id = args.get("tg_user_id", "")
-        handle = args.get("user_handle", "")
-        contact = None
+        tg_user_handle = args.get("tg_user_handle", "")
         with get_session() as session:
+            # try to find telegram contact using tg_user_id
             if tg_user_id:
-                contact = session.query(Contact).filter_by(tg_user_id=tg_user_id).first()
-            if not (tg_user_id or contact) and handle:
-                contact = session.query(Contact).filter_by(user_handle=handle).first()
+                contact = session.query(Contact).filter_by(tg_user_id=tg_user_id, source="telegram").first()
+            # try to find telegram contact using handle
+            if (not contact) and tg_user_handle:
+                contact = session.query(Contact).filter_by(user_handle=tg_user_handle, source="telegram").first()
+            # if still no contact found, create new contact
             if not contact:
-                contact = Contact(source="telegram", user_handle=handle, tg_user_id=tg_user_id, bot_activated=True)
-                with get_session() as session:
-                    session.add(contact)
-                    session.commit()
-                    session.refresh(contact)
-                    return []
+                if not (tg_user_id or tg_user_handle):
+                    return "Error: Not enough contact information"
+                contact = Contact(source="telegram", user_handle=tg_user_handle, tg_user_id=tg_user_id, bot_activated=True)
+                session.add(contact)
+                session.commit()
+                session.refresh(contact)
+                return []
+            if not contact:
+                return "Contact not found"
             demands = session.query(Demand).filter_by(contact=contact).order_by(nulls_last(desc(Demand.datetime))).limit(100).all()
             if not demands:
                 return "You have not submitted any requests. Please click on /find to submit a request"
@@ -183,13 +227,13 @@ def find_matches():
                 if not matches:
                     continue
                 supply_ids = [match.supply_id for match in matches]
-                supplies = session.query(Supply).filter(Supply.id.in_(supply_ids)).all()
+                supplies = session.query(Supply).filter(Supply.id.in_(supply_ids), Supply.phone.isnot(None)).all()
                 if not supplies:
                     return "No new results found"
-                key = ", ".join(filter(None, [demand.resource, demand.category, demand.location_text, demand.phone]))
+                key = ", ".join(filter(None, [demand.resource, demand.category, demand.location_raw, demand.phone]))
                 if not key:
                     return "Invalid request detected"
-                value = ["{}: {}".format(supply.title, supply.phone) for supply in supplies]
+                value = [": ".join(filter(None, [supply.title, supply.phone])) for supply in supplies]
                 new_matches.append(dict([(key, value)]))
                 for match in matches:
                     match.sent = True
@@ -199,11 +243,11 @@ def find_matches():
             else:
                 return "No new results found"
 
-def generate_response(results):
+def generate_response(results, status_code=400):
     """Converts results (empty list, list of vars (incl. internal vars), or string) into a response-friendly object"""
     # if result was a string, it's an error
     if isinstance(results,str):
-        return {"error": results.replace("\"", "'")}, 400
+        return {"error": results.replace("\"", "'")}, status_code
     elif not isinstance(results, list):
         return {"error": "Invalid data received from server", "data": results}, 503
     # get all vars/children of the SQLAlchemy object
@@ -225,22 +269,21 @@ def user_lookup_callback(_jwt_header, jwt_data):
 
 @app.post("/login")
 def login():
-    queries = request.args.to_dict()
-    username = queries.get("username")
-    password = queries.get("password")
-    if(username is not None and password is not None):
-        with get_session() as session:
-            user = session.query(Auth).filter_by(username=username).first()
-        if(user):
-            if(safe_str_cmp(user.password.encode('utf-8'),password.encode('utf-8'))):
-                access_token = create_access_token(identity=user)
-                return {"access_token": access_token}, 200
-            else:
-                return {"error": "Authentication failed"}, 401
+    args = request.args.to_dict()
+    username = args.get("username")
+    password = args.get("password")
+    if not (username and password):
+        return {"error": "Please provide both Username and Password"}, 401
+    with get_session() as session:
+        user = session.query(Auth).filter_by(username=username).first()
+    if(user):
+        if(safe_str_cmp(user.password.encode('utf-8'),password.encode('utf-8'))):
+            access_token = create_access_token(identity=user)
+            return {"access_token": access_token}, 200
         else:
             return {"error": "Authentication failed"}, 401
     else:
-        return {"error": "Please provide both Username and Password."},401
+        return {"error": "Authentication failed"}, 401
 
 
 @app.get("/requests")
@@ -255,13 +298,19 @@ def get_supply():
     return generate_response(results)
 
 
-@app.get("/results")
+@app.get("/location")
+def get_locations():
+    results = location_search()
+    return generate_response(results)
+
+
+@app.get("/matches")
 def get_matches():
     results = find_matches()
     return generate_response(results)
 
 
-@app.get("/raw")
+@app.get("/rawdata")
 def get_raw():
     results = search(Raw)
     return generate_response(results)
@@ -281,7 +330,14 @@ def post_supply():
     return generate_response(results)
 
 
-@app.post("/raw")
+@app.post("/volunteer")
+@jwt_required()
+def post_volunteer():
+    results = insert_or_update(Volunteer)
+    return generate_response(results)
+
+
+@app.post("/rawdata")
 @jwt_required()
 def post_raw():
     results = insert_or_update(Raw)
--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.automap import automap_base
 from credentials import USERNAME, PASSWORD, HOSTNAME, PORT, DB_NAME
 from contextlib import contextmanager
 
-engine = sqlalchemy.create_engine("postgresql+psycopg2://{}:{}@{}:{}/{}".format(USERNAME,PASSWORD,HOSTNAME,PORT,DB_NAME))
+engine = sqlalchemy.create_engine(f"postgresql+psycopg2://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DB_NAME}")
 
 metadata = sqlalchemy.MetaData(engine)
 metadata.reflect()
@@ -12,7 +12,7 @@ metadata.reflect()
 Base = automap_base(metadata=metadata)
 Base.prepare(engine)
 
-# All tables that can be queried using the API
+# All db tables can be queried using the API
 Demand = Base.classes.Demand
 Supply = Base.classes.Supply
 Raw = Base.classes.Raw
@@ -20,32 +20,14 @@ Matches = Base.classes.Matches
 Auth = Base.classes.Auth
 UserLog = Base.classes.UserLog
 Contact = Base.classes.Contact
+Locations = Base.classes.Locations
+Volunteer = Base.classes.Volunteer
 
-"""
-class Demand(Base):
-    __table__ = sqlalchemy.Table("Demand", metadata)
-
-class Supply(Base):
-    __table__ = sqlalchemy.Table("Supply", metadata)
-
-class Matches(Base):
-    __table__ = sqlalchemy.Table("Matches", metadata)
-
-class Raw(Base):
-    __table__ = sqlalchemy.Table("Raw", metadata)
-
-class Auth(Base):
-    __table__ = sqlalchemy.Table("Auth", metadata)
-
-class UserLog(Base):
-    __table__ = sqlalchemy.Table("UserLog", metadata)
-"""
 
 @contextmanager
 def get_session():
     Session = sqlalchemy.orm.sessionmaker(engine)
     session = Session()
-
     try:
         yield session
     except:
